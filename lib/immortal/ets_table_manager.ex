0abd1de2e3c8234f82515daf1fb3b7375d815efb
Daniel Berkompas <himself@danielberkompas.com>
2015-04-16 12:48:55 -0700

    Add EtsTableManager module

---
+++ lib/immortal/ets_table_manager.ex
@@ -0,0 +1,111 @@
+defmodule Immortal.EtsTableManager do
+  use GenServer
+
+  @shortdoc """
+  Keep your ETS tables alive while your process is rebooted by your supervisor.
+  """
+
+  @moduledoc """
+  Creates and keeps an ETS table alive using the `heir` feature. Normally, an
+  ETS table will die as soon as its owner process dies. This means that if the
+  owner and consumer of the table are the same process, you can lose your table
+  to a crash.
+
+  However, if (prior to its death) your table sets a different process as its 
+  heir, the crash will not delete the table. Hence, `Immortal.EtsTableManager`
+  is designed to:
+
+  1. Create an ETS table, setting itself as the heir.
+  2. Give away ownership of the table to a named process of your choosing.
+  3. If the other process crashes, receive ownership of the table back and wait
+     until that process is rebooted by the supervisor.
+  4. Give ownership back to the other process after it reboots.
+
+  ## Examples
+
+  Set up your supervisor children like this:
+
+      children = [
+        worker(YourApp.TableConsumer, []),
+        worker(Immortal.EtsTableManager, [YourApp.TableConsumer, [:public]])
+      ]
+
+  `Immortal.EtsTableManager` will wait until your `YourApp.TableConsumer`
+  process boots, and then hand over control of the table. The `[:public]`
+  argument is a configuration list that will be passed to `:ets.new/2` when
+  the ETS table is created.
+
+  The `YourApp.TableConsumer` module should be a `GenServer`, and implement
+  the following callback in order to receive control of the table:
+
+      def handle_info({:"ETS-TRANSFER", table, _manager_pid, _data}, state) do
+        new_state = ... # add table to your state
+        {:noreply, new_state}
+      end
+  """
+
+  @doc """
+  Start a new TableManager process.
+
+  - `target_process_name`: The name of the process that you want to give the
+    table away to. The process must be named so that TableManager can find it
+    again if it dies.
+
+  - `ets_options`: Any options you want to pass to `:ets.new/2`.
+  """
+  @spec start_link(atom, list) :: {:ok, pid}
+  def start_link(target_process_name, ets_options \\ []) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, :ok)
+    GenServer.call(pid, {:create_table, target_process_name, ets_options})
+    {:ok, pid}
+  end
+
+  @doc """
+  Get the ID of the ETS table that a TableManager is currently managing.
+  """
+  @spec table(pid) :: integer
+  def table(manager) do
+    GenServer.call(manager, :retrieve_table)
+  end
+
+  ###
+  # GenServer API
+  ###
+
+  @doc "Create and give away the ETS table"
+  def handle_call({:create_table, target_process_name, ets_options}, _from, _state) do
+    ets_options = ets_options ++ [{:heir, self, {}}]
+    table = :ets.new(Immortal.Table, ets_options)
+    give_away(table, target_process_name)
+    {:reply, table, {table, target_process_name}}
+  end
+
+  @doc "Retrieve the ETS table"
+  def handle_call(:retrieve_table, _from, {table,_} = state) do
+    {:reply, table, state}
+  end
+
+  @doc "Handle the ETS transfer if CallTracker crashes"
+  def handle_info({:"ETS-TRANSFER", table, _pid, _data}, {_, target_process_name} = state) do
+    give_away(table, target_process_name)
+    {:noreply, state}
+  end
+
+  ###
+  # Private API
+  ###
+
+  defp give_away(table, target_process_name) do
+    pid = wait_for(target_process_name)
+    :ets.give_away(table, pid, {})
+  end
+
+  defp wait_for(module) do
+    case Process.whereis(module) do
+      nil ->
+        :timer.sleep(1)
+        wait_for(module)
+      pid -> pid
+    end
+  end
+end
---
+++ test/immortal/ets_table_manager_test.exs
@@ -0,0 +1,71 @@
+defmodule Immortal.EtsTableManagerTest do
+  use ExUnit.Case
+  alias Immortal.EtsTableManager, as: TableManager
+
+  defmodule TableConsumer do
+    use GenServer
+
+    def start do
+      GenServer.start(__MODULE__, :ok, name: __MODULE__)
+    end
+
+    def restart do
+      process = Process.whereis(__MODULE__)
+      Process.exit(process, :normal)
+      start
+    end
+
+    def table do
+      GenServer.call(__MODULE__, :retrieve_table)
+    end
+
+    ###
+    # GenServer API
+    ###
+
+    def handle_call(:retrieve_table, _from, table) do
+      {:reply, table, table}
+    end
+
+    def handle_info({:"ETS-TRANSFER", table, _pid, _data}, _state) do
+      {:noreply, table}
+    end
+  end
+
+  setup do
+    TableConsumer.start
+    {:ok, manager} = TableManager.start_link(TableConsumer, [:public])
+    {:ok, manager: manager}
+  end
+
+  test "ETS table attributes can be customized when manager starts" do
+    {:ok, manager} = TableManager.start_link(TableConsumer, [:ordered_set])
+    table = TableManager.table(manager)
+    consumer = Process.whereis(TableConsumer)
+    info = :ets.info(table)
+
+    assert info[:type]  == :ordered_set
+    assert info[:owner] == consumer
+    assert info[:heir]  == manager
+  end
+
+  test "can provide an ETS table to a properly configured consumer", context do
+    managed_table = TableManager.table(context[:manager])
+    assert is_integer(managed_table)
+    assert TableConsumer.table == managed_table
+  end
+
+  test "the table survives restarts of the consumer" do
+    # Insert some data
+    table = TableConsumer.table
+    :ets.insert(table, {:test, "value"})
+
+    # Restart the TableConsumer process
+    TableConsumer.restart
+    table = TableConsumer.table
+
+    # Ensure data is still present
+    [value] = :ets.lookup(table, :test)
+    assert value == {:test, "value"}
+  end
+end
